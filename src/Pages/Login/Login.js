ddfb012380e22ad102248d5d9ac99d80b450740c
Saadi-Rahman <sadi6280@gmail.com>
2022-11-19 06:51:05 +0600

    added react-hook-form and using form validation

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,46 +1,58 @@
+import React from 'react';
 import { useForm } from 'react-hook-form';
 import { Link } from 'react-router-dom';
 
 const Login = () => {
-    const {register, handleSubmit} = useForm();
+    const {register, formState: { errors }, handleSubmit} = useForm();
     const handleLogin = data => {
         console.log(data);
     }
 
     return (
-        <div>
-            <div className="hero min-h-screen">
-                <div className="card w-full max-w-sm shadow-2xl bg-base-100">
-                    <div className="card-body">
-                        <h3 className='text-3xl font-bold text-center'>Login</h3>
-                        
-                        <form onSubmit={handleSubmit(handleLogin)}>
-                            <div className="form-control">
-                                <label className="label">
-                                    <span className="label-text">Email</span>
-                                </label>
-                                <input {...register("email")} type="email" placeholder="Enter your email" className="input input-bordered" required />
-                            </div>
-                            <div className="form-control">
-                                <label className="label">
-                                    <span className="label-text">Password</span>
-                                </label>
-                                <input {...register("password")} type="password" placeholder="Enter your password" className="input input-bordered" required />
-                                <label className="label">
-                                    <Link to="#" className="label-text-alt link link-hover">Forgot password?</Link>
-                                </label>
-                            </div>
-                            <div className="form-control mt-2">
-                                <input type="submit" value="Login" className="btn btn-accent" />
-                            </div>
-                            <label className="label py-0">
-                                <small>New to Doctor's Portal? <Link to="/register" className="link link-hover text-secondary">Create an Account</Link></small>
-                            </label>
-                        </form>
-                        <div className="divider my-0">OR</div>
-                        <div className="form-control mt-2">
-                            <button className="btn btn-outline btn-accent">Continue with Google</button>
+        <div className="hero min-h-screen">
+            <div className="card w-full max-w-sm shadow-2xl bg-base-100">
+                <div className="card-body">
+                    <h3 className='text-3xl font-bold text-center'>Login</h3>
+                    
+                    <form onSubmit={handleSubmit(handleLogin)}>
+                        <div className="form-control">
+                            <label className="label"><span className="label-text">Email</span></label>
+                            <input type="email" placeholder="Enter your email" className="input input-bordered"
+                                {...register("email", {
+                                    required: "Email Address is required!"
+                                })} 
+                            />
+                            {errors.email && <small className='text-red-500'>{errors.email?.message}</small>}
                         </div>
+
+                        <div className="form-control">
+                            <label className="label"><span className="label-text">Password</span></label>
+                            <input type="password" placeholder="Enter your password" className="input input-bordered"
+                                {...register("password", {
+                                    required: "Password is required!",
+                                    minLength: {value: 6, message: "Password must be 6 characters or longer!"}
+                                })} 
+                            />
+                            {errors.password && <small className='text-red-500'>{errors.password?.message}</small>}
+                        </div>
+
+                        <label className="label">
+                            <Link to="#" className="label-text-alt link link-hover">Forgot password?</Link>
+                        </label>
+
+                        <div className="form-control mt-2">
+                            <input type="submit" value="Login" className="btn btn-accent" />
+                        </div>
+
+                        <label className="label py-0">
+                            <small>New to Doctor's Portal? <Link to="/signup" className="link link-hover text-secondary">Create an Account</Link></small>
+                        </label>
+                    </form>
+
+                    <div className="divider my-0">OR</div>
+
+                    <div className="form-control mt-2">
+                        <button className="btn btn-outline btn-accent">Continue with Google</button>
                     </div>
                 </div>
             </div>
---
+++ src/Pages/SignUp/SignUp.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import { useForm } from 'react-hook-form';
+import { Link } from 'react-router-dom';
+
+const SignUp = () => {
+
+    const {register, formState: {errors}, handleSubmit} = useForm();
+
+    const handleSignUp = (data) => {
+        console.log(data);
+    }
+
+    return (
+        <div className="hero min-h-screen">
+            <div className="card w-full max-w-sm shadow-2xl bg-base-100">
+                <div className="card-body">
+                    <h3 className='text-3xl font-bold text-center'>Sign Up</h3>
+                    
+                    <form onSubmit={handleSubmit(handleSignUp)}>
+                        <div className="form-control">
+                            <label className="label"><span className="label-text">Name</span></label>
+                            <input type="text" placeholder="Enter your Name" className="input input-bordered"
+                                {...register("name", {
+                                    required: "Name is required!"
+                                })}
+                            />
+                            {errors.name && <small className='text-red-500'>{errors.name.message}</small>}
+                        </div>
+
+                        <div className="form-control">
+                            <label className="label"><span className="label-text">Email</span></label>
+                            <input type="email" placeholder="Enter your email" className="input input-bordered"
+                                {...register("email", {
+                                    required: "Email is required!"
+                                })}
+                            />
+                            {errors.email && <small className='text-red-500'>{errors.email.message}</small>}
+                        </div>
+
+                        <div className="form-control">
+                            <label className="label"><span className="label-text">Password</span></label>
+                            <input type="password" placeholder="Enter your password" className="input input-bordered"
+                                {...register("password", {
+                                    required: "Password is required",
+                                    minLength: {value: 6, message: "Password must be 6 characters or longer!"},
+                                    pattern: {value: /(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])(?=.*[a-z])/, message: "Password must have a special character with a lower case, upper case and a number!"}
+                                })}
+                            />
+                            {errors.password && <small className='text-red-500'>{errors.password.message}</small>}
+                        </div>
+                        
+                        <div className="form-control mt-6">
+                            <input type="submit" value="Sign Up" className="btn btn-accent" />
+                        </div>
+
+                        <label className="label py-0">
+                            <small>Already have an Account? <Link to="/login" className="link link-hover text-secondary">Please Login</Link></small>
+                        </label>
+                    </form>
+
+                    <div className="divider my-0">OR</div>
+
+                    <div className="form-control mt-2">
+                        <button className="btn btn-outline btn-accent">Continue with Google</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default SignUp;
